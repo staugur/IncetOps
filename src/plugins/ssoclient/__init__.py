6bb2b26704b4dcd322eedbff28fbfd7ebfb8c07b
Mr.tao <staugur@saintic.com>
2019-01-07 11:59:14 +0800

    fix version

--- src/plugins/ssoclient/__init__.py
+++ src/plugins/ssoclient/__init__.py
@@ -32,7 +32,7 @@ __description__ = "SSO Client"
 __author__      = "Mr.tao <staugur@saintic.com>"
 #: Plugin Version
 #: 插件版本
-__version__     = "0.1" 
+__version__     = "0.1.0"
 #: Plugin Url
 #: 插件主页
 __url__         = "https://www.saintic.com"
